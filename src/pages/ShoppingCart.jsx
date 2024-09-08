c925dc04b4af301a89dfb887e19d77d458484539
Sidharth R P <sidharthrprakash@gmail.com>
2024-09-08 13:38:21 +0530

    Shopping cart design change

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 function ShoppingCart() {
   const [items, setItems] = useState();
   let totalPrice = 0
+  let discount = 1
+  let finalPrice = 0
+  let delivery = 10
   useEffect(()=>{
     axios.get(`https://fakestoreapi.com/products?limit=2`)
     .then((res)=>{
@@ -18,8 +21,10 @@ function ShoppingCart() {
             {items ? <div className='my-10'>
                 {items.map((item,index)=>{
                     totalPrice+=parseInt(item.price)
+                    {discount=Math.floor(totalPrice*.1)}
+                    {finalPrice=totalPrice-discount+delivery}
                     return(
-                    <div className='flex gap-10 my-10 border-2 rounded-xl p-2' key={index}>
+                    <div className='flex gap-10 my-10  rounded-xl p-2 shadow-lg' key={index}>
                     <img className=' h-30 w-20' src={item.image}></img>
                     <div>
                         <h1 className='font-bold'>{item.title}</h1>
@@ -31,13 +36,34 @@ function ShoppingCart() {
                 )
                 }
                 )}
-            <button className='bg-green-600 rounded-lg p-2 text-white'>Proceed to Buy</button>
+            <button className='bg-green-600 rounded-lg p-2 text-white hover:text-black shadow-lg'>Proceed to Buy</button>
             </div>: <p>No Items in Cart</p>}
         </div>
-        <div className='border-2 p-10'>
-            <h1 className='p-10 font-bold'>Price Details</h1>
-            <p className='py-2'>Price({items&& items.length} items) </p>
-            <h1 className='py-2 font-bold'>$ {totalPrice}</h1>
+        
+        <div className=' p-10 '>
+            <div className='rounded-xl p-5 shadow-lg'>
+                <h1 className='p-5 text-xl'>Price Details</h1>
+                <div className='flex justify-between'>
+                    <p className='py-2'>Price({items&& items.length} items) </p>
+                    <h1 className='py-2 font-bold'>${totalPrice}</h1>
+                </div>
+                <div className='flex justify-between'>
+                    <p className='py-2'>Discount </p>
+                    
+                    <h1 className='py-2 font-bold'>-${discount}</h1>
+                </div>
+                <div className='flex justify-between'>
+                    <p className='py-2'>Delivery Charge </p>
+                    <h1 className='py-2 font-bold'>${delivery}</h1>
+                </div>
+                <hr></hr>
+                <div className='flex pt-3 justify-between'>
+                    <p className='py-2'>Final Price </p>
+                    
+                    <h1 className='py-2 font-bold text-green-500'>${finalPrice}</h1>
+                </div>
+            
+            </div>
         </div>
     </div>
   )
